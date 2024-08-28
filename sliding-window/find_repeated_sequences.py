d173ae721066bb1ba1f783c1e14a63c7535c2bce
Lester <lgdelacruz92@gmail.com>
2024-08-28 09:27:36 -0400

    expected and result must match

--- sliding-window/find_repeated_sequences.py
+++ sliding-window/find_repeated_sequences.py
@@ -1,2 +1,11 @@
-def test_find_repeated_sequences():
-    assert True == False
+def find_repeated_sequences(dna, k):
+
+    # Replace this placeholder return statement with your code
+    ans = set()
+    record = set()
+    for i in range(len(dna)-k):
+        substr = dna[i:i+k]
+        if substr in record:
+            ans.add(substr)
+        record.add(substr)
+    return list(ans)
--- sliding-window/test_find_repeated_sequences.py
+++ sliding-window/test_find_repeated_sequences.py
@@ -1,2 +1,8 @@
+from find_repeated_sequences import find_repeated_sequences
 def test_find_repeated_sequences():
-    assert True == False
+    expected = ["AAAAACCC","AAAACCCC","AAACCCCC"]
+    expected.sort()
+    result = find_repeated_sequences("AAAAACCCCCAAAAACCCCCC", 8)
+    result.sort()
+    assert len(result) == len(expected)
+    assert result == expected
