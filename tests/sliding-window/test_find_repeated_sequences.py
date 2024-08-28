9bbb727f848e4457564e737b3a62217df7d837c0
Lester <lgdelacruz92@gmail.com>
2024-08-28 10:09:41 -0400

    restructure

--- tests/sliding-window/test_find_repeated_sequences.py
+++ tests/sliding-window/test_find_repeated_sequences.py
@@ -1,4 +1,5 @@
-from find_repeated_sequences import find_repeated_sequences
+from sliding_window.find_repeated_sequences import find_repeated_sequences
+
 def test_find_repeated_sequences_1():
     k = 8
     dna = "AAAAACCCCCAAAAACCCCCC"
