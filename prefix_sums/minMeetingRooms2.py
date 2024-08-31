6378c6f6d0443cc2e612fb04815c529f79375a01
Lester <lgdelacruz92@gmail.com>
2024-08-31 11:34:20 -0400

    min meeting rooms

---
+++ prefix_sums/minMeetingRooms2.py
@@ -0,0 +1,5 @@
+from typing import List
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        return 0
---
+++ tests/prefix_sums/test_minMeetingRooms2.py
@@ -0,0 +1,20 @@
+from prefix_sums.minMeetingRooms2 import Solution
+
+def test_minMeetingRooms2_1():
+    intervals = [[0,30],[5,10],[15,20]]
+
+    solution = Solution()
+    expected = 2
+
+    result = solution.minMeetingRooms(intervals)
+    assert result == expected
+
+def test_minMeetingRooms2_2():
+    intervals = [[7,10],[2,4]]
+
+
+    solution = Solution()
+    expected = 1
+
+    result = solution.minMeetingRooms(intervals)
+    assert result == expected
