44e8982a4b6d52645af182c7cd9e73c65b024cd4
Lester <lgdelacruz92@gmail.com>
2024-08-31 10:23:30 -0400

    prefix sums

--- tests/sliding_window/test_numberOfSpecialSubstrings.py
+++ tests/sliding_window/test_numberOfSpecialSubstrings.py
@@ -1,10 +1,28 @@
 from sliding_window.numberOfSpecialSubstrings import Solution
 
-def test_lengthOfLongestSubstringTwoDistinct_1():
+def test_numberOfSpecialSubstrings_1():
     solution = Solution()
     s = 'ooo'
 
     expected = 3
+
+    result= solution.numberOfSpecialSubstrings(s)
+    assert result == expected
+
+def test_numberOfSpecialSubstrings_2():
+    solution = Solution()
+    s = 'abcd'
+
+    expected = 10
+
+    result= solution.numberOfSpecialSubstrings(s)
+    assert result == expected
+
+def test_numberOfSpecialSubstrings_3():
+    solution = Solution()
+    s = 'abab'
+
+    expected = 7
 
     result= solution.numberOfSpecialSubstrings(s)
     assert result == expected
