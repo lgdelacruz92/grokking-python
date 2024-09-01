af002e3d1cd0ac08d728dedb28435d22a1c0cbd5
Lester <lgdelacruz92@gmail.com>
2024-09-01 08:47:20 -0400

    okay

--- shortest_paths/jumpGame2.py
+++ shortest_paths/jumpGame2.py
@@ -1,4 +1,25 @@
 from typing import List
+from collections import deque
+
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        return 0
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 0
+        
+        cur_index = 0
+        count = 0
+        while cur_index != n-1:
+            if cur_index + nums[cur_index] >= n-1:
+                return count + 1
+            max_next_index = 0
+            cur_max = 0
+            count += 1
+            for next_index in range(cur_index+1, cur_index + nums[cur_index] + 1):
+                if next_index + nums[next_index] > cur_max:
+                    cur_max = next_index + nums[next_index]
+                    max_next_index = next_index
+            cur_index = max_next_index
+        return count
